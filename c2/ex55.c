7baae42083585731daa4dca7e8184eb9fba287ed
aprz512 <lyldalek@gmail.com>
2022-04-06 11:30:11 +0800

    ex2.56

--- c2/ex55.c
+++ c2/ex55.c
@@ -2,29 +2,33 @@
 
 typedef unsigned char *byte_pointer;
 
-void show_bytes(byte_pointer start, size_t len) {
+void show_bytes(byte_pointer start, size_t len)
+{
     size_t i;
-    for ( i = 0; i < len; i++)
+    for (i = 0; i < len; i++)
     {
         printf(" %.2x", start[i]);
     }
     printf("\n");
-    
 }
 
-void show_int(int x) {
-    show_bytes((byte_pointer) &x, sizeof(int));
+void show_int(int x)
+{
+    show_bytes((byte_pointer)&x, sizeof(int));
 }
 
-void show_float(float x) {
-    show_bytes((byte_pointer) &x, sizeof(int));
+void show_float(float x)
+{
+    show_bytes((byte_pointer)&x, sizeof(int));
 }
 
-void show_point(void* x) {
-    show_bytes((byte_pointer) &x, sizeof(int));
+void show_point(void *x)
+{
+    show_bytes((byte_pointer)&x, sizeof(int));
 }
 
-int main() {
+int main()
+{
 
     int x = 1;
     // 结果输出 01 00 00 00
---
+++ c2/ex56.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++)
+    {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x)
+{
+    show_bytes((byte_pointer)&x, sizeof(int));
+}
+
+void show_float(float x)
+{
+    show_bytes((byte_pointer)&x, sizeof(int));
+}
+
+void show_point(void *x)
+{
+    show_bytes((byte_pointer)&x, sizeof(int));
+}
+
+int main()
+{
+
+    show_int(1);
+    //  00 00 80 3f，这个可以自己算一下是否符合期望，1 + 8 + 23
+    show_float(1.0f);
+    int p = 0;
+    show_point(&p);
+    return 0;
+}
