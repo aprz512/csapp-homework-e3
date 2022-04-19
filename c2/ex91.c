75298f4bf6d47cf04a97fc95e790aa8d268d8571
aprz512 <lyldalek@gmail.com>
2022-04-19 23:35:56 +0800

    ex91

---
+++ c2/ex91.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+  // A
+  // 0 10000000 10010010000111111011011
+  // E = 1,
+  // 0b11.0010010000111111011011
+
+  // B
+  // 0b11.001001(001)...
+
+  // C
+  // 第9位
+
+  return 0;
+}
