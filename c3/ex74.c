62362de5211f65466b26f0a70f291b8061207b4e
liyunlei <liyunlei.a5right@bytedance.com>
2022-10-04 08:45:30 +0800

    ex74 -

---
+++ c3/ex74.c
@@ -0,0 +1,18 @@
+/**
+ * @file ex74.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * 
+Write a function in assembly code that matches the behavior of the function find_
+range in Figure 3.51. Your code should contain only one floating-point comparison
+instruction, and then it should use conditional moves to generate the correct result.
+You might want to make use of the instruction cmovp (move if even parity). Test
+your code on all 2^32 possible argument values. Web Aside asm:easm on page 214
+describes how to incorporate functions written in assembly code into C programs.
+ * 
+ */
